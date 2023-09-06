1b935e94e9a3128ed63ce6ca0eb3c8d219cddf80
Martin Průša <prusamartin.pm@gmail.com>
2023-09-06 15:57:39 +0200

    create matrix test

--- src/test/java/cz/educanet/matrices/CreateMatrixTest.java
+++ src/test/java/cz/educanet/matrices/CreateMatrixTest.java
@@ -11,9 +11,22 @@ class CreateMatrixTest {
 
     @Test
     public void should_Succeed_When_NumberOnCoordinatesIsValid() {
-        double[][] d = {{3, 2, 4}, {9, 8, 7}, {23, 33, 43}};
-        IMatrix m = matrixFactory.create(d);
-        Utils.matrixEquals(m, d);
+        double[][][] data = {{{3, 2, 4}, {9, 8, 7}, {23, 33, 43}}, {{2, 3, 5, 6}, {4, 9, 3, 2}}};
+
+        for (double[][] d : data) {
+            IMatrix m = matrixFactory.create(d);
+            Utils.matrixEquals(m, d);
+        }
+    }
+
+    @Test
+    public void should_Succeed_When_SizeIsCorrect() {
+        double[][][] data = {{{3, 2, 4}, {9, 8, 7}, {23, 33, 43}}, {{2, 3, 5, 6}, {4, 9, 3, 2}}};
+        for (double[][] d : data) {
+            IMatrix m = matrixFactory.create(d);
+            Assertions.assertEquals(d.length, m.getRows());
+            if(d[0] != null) Assertions.assertEquals(d[0].length, m.getColumns());
+        }
     }
 
 }
