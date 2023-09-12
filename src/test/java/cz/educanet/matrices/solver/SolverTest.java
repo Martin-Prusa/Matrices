10762f981b59868094f3926d1796de6f732125cf
Martin Průša <prusamartin.pm@gmail.com>
2023-09-12 13:19:31 +0200

    better test

--- src/test/java/cz/educanet/matrices/solver/SolverTest.java
+++ src/test/java/cz/educanet/matrices/solver/SolverTest.java
@@ -16,16 +16,18 @@ class SolverTest {
     private final Solver solver = new Solver();
 
     @Test
-    public void should_SolveEquations_When__MatrixIsSquare() {
+    public void should_SolveEquations_When_MatrixIsSquare() {
         IMatrix[] matrices = new IMatrix[]{
-                matrixFactory.create(new double[][]{{1,2},{4,3}})
+                matrixFactory.create(new double[][]{{1, 2}, {4, 3}}),
+                matrixFactory.create(new double[][]{{4, 3, 0}, {2, 2, -2}, {5, 3, 1}})
         };
 
         IMatrix[] solutions = new IMatrix[]{
-                matrixFactory.create(new double[][]{{5}, {15}})
+                matrixFactory.create(new double[][]{{5}, {15}}),
+                matrixFactory.create(new double[][]{{4}, {0}, {-2}})
         };
 
-        double[][] results = new double[][]{{3,1}};
+        double[][] results = new double[][]{{3, 1}, {-11, 16, 5}};
 
         for (int i = 0; i < matrices.length; i++) {
             double[] r = solver.solve(matrices[i], solutions[i]);
